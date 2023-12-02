9f1b20b9dab172eeb3626ab7728084cf9818353b
Shuhao Zhang <shuhaozhangchris@gmail.com>
2023-12-02 14:44:17 -0500

    configure identitySvc for next app client

--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -32,34 +32,18 @@ public static class Config
                   new Secret("NotASecret".Sha256())
                 },
               AllowedGrantTypes = { GrantType.ResourceOwnerPassword }
+            },
+            new Client
+            {
+              ClientId = "nextApp",
+              ClientName = "nextApp",
+              ClientSecrets = { new Secret("secret".Sha256()) },
+              AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+              RequirePkce = false,
+              RedirectUris = { "http://localhost:3000/api/auth/callback/id-server" },
+              AllowOfflineAccess = true,
+              AllowedScopes = { "openid", "profile", "auctionApp" },
+              AccessTokenLifetime = 3600 * 24 * 30 // token lasts for a month
             }
-
-            // m2m (machine to machine) client credentials flow client
-            // new Client
-            // {
-            //     ClientId = "m2m.client",
-            //     ClientName = "Client Credentials Client",
-
-            //     AllowedGrantTypes = GrantTypes.ClientCredentials,
-            //     ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
-
-            //     AllowedScopes = { "scope1" }
-            // },
-
-            // interactive client using code flow + pkce
-            // new Client
-            // {
-            //     ClientId = "interactive",
-            //     ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
-
-            //     AllowedGrantTypes = GrantTypes.Code,
-
-            //     RedirectUris = { "https://localhost:44300/signin-oidc" },
-            //     FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-            //     PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
-
-            //     AllowOfflineAccess = true,
-            //     AllowedScopes = { "openid", "profile", "scope2" }
-            // },
         };
 }
