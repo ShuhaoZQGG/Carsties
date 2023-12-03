3ece4085beee03ea53c64643143246f646fe527f
Shuhao Zhang <shuhaozhangchris@gmail.com>
2023-12-02 20:04:39 -0500

    add gateway-svc to container

--- src/IdentityService/Pages/Diagnostics/Index.cshtml.cs
+++ src/IdentityService/Pages/Diagnostics/Index.cshtml.cs
@@ -13,11 +13,11 @@ public class Index : PageModel
         
     public async Task<IActionResult> OnGet()
     {
-        var localAddresses = new string[] { "127.0.0.1", "::1", HttpContext.Connection.LocalIpAddress.ToString() };
-        if (!localAddresses.Contains(HttpContext.Connection.RemoteIpAddress.ToString()))
-        {
-            return NotFound();
-        }
+        // var localAddresses = new string[] { "127.0.0.1", "::1", HttpContext.Connection.LocalIpAddress.ToString() };
+        // if (!localAddresses.Contains(HttpContext.Connection.RemoteIpAddress.ToString()))
+        // {
+        //     return NotFound();
+        // }
 
         View = new ViewModel(await HttpContext.AuthenticateAsync());
             
