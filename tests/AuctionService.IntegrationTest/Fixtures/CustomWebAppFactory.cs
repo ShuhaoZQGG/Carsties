f2412ac86b5af2010a0d345f019dde37f9a3ca98
Shuhao Zhang <shuhaozhangchris@gmail.com>
2023-12-04 09:38:44 -0500

    set up for integration tests

---
+++ tests/AuctionService.IntegrationTest/Fixtures/CustomWebAppFactory.cs
@@ -0,0 +1,38 @@
+﻿using AuctionService.Data;
+using MassTransit;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Testcontainers.PostgreSql;
+
+namespace AuctionService.IntegrationTest;
+public class CustomWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+  private PostgreSqlContainer _postgreSqlContainer = new PostgreSqlBuilder().Build();
+  public async Task InitializeAsync()
+  {
+    await _postgreSqlContainer.StartAsync();
+  }
+
+  protected override void ConfigureWebHost(IWebHostBuilder builder)
+  {
+    // Remove AuctionDbContext in AuctionService, and replace with the test one.
+    builder.ConfigureTestServices(services => 
+    {
+      services.RemoveContext<AuctionDbContext>();
+
+
+      services.AddDbContext<AuctionDbContext>(options => 
+      {
+        options.UseNpgsql(_postgreSqlContainer.GetConnectionString());
+      });
+      services.AddMassTransitTestHarness();
+      services.EnsureCreated<AuctionDbContext>();
+    });
+    base.ConfigureWebHost(builder);
+  }
+
+  Task IAsyncLifetime.DisposeAsync() => _postgreSqlContainer.DisposeAsync().AsTask();
+}
---
+++ tests/AuctionService.IntegrationTest/Utils/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuctionService.IntegrationTest;
+public static class ServiceCollectionExtensions
+{
+  public static void RemoveContext<T>(this IServiceCollection services)
+  {
+    var descriptor = services.SingleOrDefault(
+        d => d.ServiceType == typeof(DbContextOptions<AuctionDbContext>)
+    );
+
+    if (descriptor != null) services.Remove(descriptor);
+  }
+
+  public static void EnsureCreated<T>(this IServiceCollection services)
+  {
+      var sp = services.BuildServiceProvider();
+      using var scope = sp.CreateScope();
+      var scopedServices = scope.ServiceProvider;
+      var db = scopedServices.GetRequiredService<AuctionDbContext>();
+      db.Database.Migrate();
+      DbHelper.InitDbForTests(db);
+  }
+}
