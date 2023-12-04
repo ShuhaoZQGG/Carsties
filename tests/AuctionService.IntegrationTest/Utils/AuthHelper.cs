915ef4e6e482de078ed38b1694b9bc86917d25f0
Shuhao Zhang <shuhaozhangchris@gmail.com>
2023-12-04 11:24:33 -0500

    integration tests for controller methods

---
+++ tests/AuctionService.IntegrationTest/Utils/AuthHelper.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+
+namespace AuctionService.IntegrationTest;
+public static class AuthHelper
+{
+  public static Dictionary<string, object> GetBearerForUser(string username)
+  {
+    return new Dictionary<string, object>{{ ClaimTypes.Name, username }};
+  }
+}
