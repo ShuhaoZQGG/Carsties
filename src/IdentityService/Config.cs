c25b62a8363dbb13a26b5822f74430e97cb06a40
Shuhao Zhang <shuhaozhangchris@gmail.com>
2023-12-19 15:47:09 -0500

    set up nginx ingress, configure ssl for nginx

--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -17,7 +17,7 @@ public static class Config
             new ApiScope("auctionApp", "Auction app full access"),
         };
 
-    public static IEnumerable<Client> Clients =>
+    public static IEnumerable<Client> Clients(IConfiguration config) =>
         new Client[]
         {
             new Client
@@ -40,7 +40,7 @@ public static class Config
               ClientSecrets = { new Secret("secret".Sha256()) },
               AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
               RequirePkce = false,
-              RedirectUris = { "http://localhost:3000/api/auth/callback/id-server" },
+              RedirectUris = { config["ClientApp"] + "/api/auth/callback/id-server" },
               AllowOfflineAccess = true,
               AllowedScopes = { "openid", "profile", "auctionApp" },
               AccessTokenLifetime = 3600 * 24 * 30, // token lasts for a month
--- src/IdentityService/HostingExtensions.cs
+++ src/IdentityService/HostingExtensions.cs
@@ -36,7 +36,7 @@ internal static class HostingExtensions
             })
             .AddInMemoryIdentityResources(Config.IdentityResources)
             .AddInMemoryApiScopes(Config.ApiScopes)
-            .AddInMemoryClients(Config.Clients)
+            .AddInMemoryClients(Config.Clients(builder.Configuration))
             .AddAspNetIdentity<ApplicationUser>()
             .AddProfileService<CustomProfileService>();
         
