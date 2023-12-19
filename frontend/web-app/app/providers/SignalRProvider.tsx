42249e487a8313d554885df97dac5924236cb800
Shuhao Zhang <shuhaozhangchris@gmail.com>
2023-12-19 16:22:28 -0500

    change network configuration to solve signalR connection issue

--- frontend/web-app/app/providers/SignalRProvider.tsx
+++ frontend/web-app/app/providers/SignalRProvider.tsx
@@ -19,10 +19,12 @@ export default function SignalRProvider({ children, user }: Props) {
   const [connection, setConnection] = useState<HubConnection | null>(null);
   const setCurrentPrice = useAuctionStore(state => state.setCurrentPrice);
   const addBid = useBidStore(state => state.addBid);
-
+  const apiUrl = process.env.NODE_ENV === 'production'
+    ? 'https://api.carsties.com/notifications'
+    : process.env.NEXT_PUBLIC_NOTIFY_URL
   useEffect(() => {
     const newConnection = new HubConnectionBuilder()
-                            .withUrl(process.env.NEXT_PUBLIC_NOTIFY_URL!)
+                            .withUrl(apiUrl!)
                             .withAutomaticReconnect()
                             .build();
     setConnection(newConnection);
