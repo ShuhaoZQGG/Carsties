8ce28c156dd835c74822a51acfece54aeb9f9e57
Shuhao Zhang <shuhao.qgg.zhang@gmail.com>
2025-06-30 07:18:45 -0400

    chore: scaffold src/Carsties.Tests/GroqAiApiTests.cs

---
+++ src/Carsties.Tests/GroqAiApiTests.cs
@@ -0,0 +1,29 @@
+using Carsties.Tests;
+using GroqAiApi;
+
+namespace Carsties.Tests
+{
+    public class GroqAiApiTests
+    {
+        // TODO: Initialize GroqAiApi instance
+        private readonly IGroqAiApi _groqAiApi;
+
+        public GroqAiApiTests(IGroqAiApi groqAiApi)
+        {
+            _groqAiApi = groqAiApi;
+        }
+
+        // TODO: Write tests for GroqAiApi methods
+        [Fact]
+        public async Task TestGroqAiApiMethod1()
+        {
+            // TODO: Implement test logic
+        }
+
+        [Fact]
+        public async Task TestGroqAiApiMethod2()
+        {
+            // TODO: Implement test logic
+        }
+    }
+}
